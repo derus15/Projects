aec56558a9cfc523e851eb028a1a653ffe6507f7
Ivan <89651706+derus15@users.noreply.github.com>
2021-12-25 20:26:44 +0400

    Delete p1.py

--- p1.py
+++
@@ -1,8 +0,0 @@
-x = int (input())
-while x > 0:
-    y = x 
-    while y > 0:
-        y-=1
-        print (y)
-    x-=1 # почему x что то значит если y взял у него инпут значение
-print ("Stopped, please")
