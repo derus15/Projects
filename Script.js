7246b7d52892e8f064acb85602026cf74911b1c5
Ivan Prikazchikov <89651706+derus15@users.noreply.github.com>
2022-10-12 18:58:49 +0400

    Add lvl counter and fix bug with first enemy

--- Script.js
+++ Script.js
@@ -1,15 +1,18 @@
 let button_1 = document.querySelector('.button_1')
 let class_1 = document.querySelector('.score_num')
-let hide = document.querySelector('.hide')
 let img = document.querySelector('img')
-let en_score = document.querySelector('.en_score')
+let en_score = document.querySelector('.en_num')
+let level_num = document.querySelector('.level_num')
+
 
 button_1.onclick = function() {
-  class_1.textContent ++;
-   if (class_1.textContent === '10') {
-    img.classList.add('hide')
-    en_score.textContent ++ ;
-
-  }
-};
+    let counter = class_1.textContent
+    class_1.textContent++;
+    if (counter % 10 === 0 && counter !== '0') {
+        img.classList.toggle('hide')
+        en_score.textContent++;}
+    if (counter % 50 === 0 && counter !== '0') {
+        level_num.textContent++;
+        }
+}
 
