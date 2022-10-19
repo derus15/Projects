1688892e2feeaf2f3f898ecd75cb442e869c2b5c
Ivan Prikazchikov <89651706+derus15@users.noreply.github.com>
2022-10-20 00:42:29 +0400

    Rename let

--- Script.js
+++ Script.js
@@ -1,15 +1,15 @@
 let button_1 = document.querySelector('.button_1')
-let class_1 = document.querySelector('.score_num')
-let img = document.querySelector('img')
+let score_num = document.querySelector('.score_num')
+let figure = document.querySelector('figure')
 let en_score = document.querySelector('.en_num')
 let level_num = document.querySelector('.level_num')
 
 
 button_1.onclick = function() {
-    let counter = class_1.textContent
-    class_1.textContent++;
+    let counter = score_num.textContent
+    score_num.textContent++;
     if (counter % 10 === 0 && counter !== '0') {
-        img.classList.toggle('hide')
+        figure.classList.toggle('hide')
         en_score.textContent++;}
     if (counter % 50 === 0 && counter !== '0') {
         level_num.textContent++;
